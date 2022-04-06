44827d74f80e948dc1fb0b9de0b5cdbbf3d31877
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2022-04-06 22:31:17 +0200

    Fix description

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
+      description="SIP-HASS Docs">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
